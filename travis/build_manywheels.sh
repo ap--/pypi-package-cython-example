bf859efbcb4cd7a2f1628a7c1dc1677cdd72bb97
ap-- <andreas@poehlmann.io>
2019-08-13 22:55:06 +0200

    include manylinux

---
+++ travis/build_manywheels.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e -x
+
+# Compile wheels
+for PYBIN in /opt/python/*/bin; do
+    "${PYBIN}/pip" wheel /io/ -w wheelhouse/
+done
+
+# Bundle external shared libraries into the wheels
+for whl in wheelhouse/*.whl; do
+    auditwheel repair "$whl" --plat $PLAT -w /io/wheelhouse/
+done
+
+# Install packages and test
+for PYBIN in /opt/python/*/bin/; do
+    echo $(ls wheelhouse/*.whl)
+done
