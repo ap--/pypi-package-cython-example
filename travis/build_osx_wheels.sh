11d50b399ec66a0310d20d61b9b51234582a9428
ap-- <andreas@poehlmann.io>
2019-08-20 22:39:05 +0200

    build relevant osx wheels using code from matthew-brett's multibuild

--- travis/build_osx_wheels.sh
+++ travis/build_osx_wheels.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+curl https://github.com/matthew-brett/multibuild/blob/ed1cfaf365ed51bd309a80e60736915d3ec70d31/osx_utils.sh -s -o osx_utils.sh
+source osx_utils.sh
+
+get_macpython_environment ${PYTHON_VERSION} venv "10.6"
+
 echo "Building wheel"
 python --version
 pip --version
 pip wheel . -w wheelhouse/
 
-# should use delocate on wheels ??
+repair_wheelhouse ./wheelhouse
