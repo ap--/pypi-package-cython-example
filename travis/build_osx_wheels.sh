391c3f47fb8a6330f065eb980c53825ea75e334f
ap-- <andreas@poehlmann.io>
2019-08-20 22:45:41 +0200

    fix osx_utils.sh download

--- travis/build_osx_wheels.sh
+++ travis/build_osx_wheels.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-curl https://github.com/matthew-brett/multibuild/blob/ed1cfaf365ed51bd309a80e60736915d3ec70d31/osx_utils.sh -s -o osx_utils.sh
+curl https://raw.githubusercontent.com/matthew-brett/multibuild/ed1cfaf365ed51bd309a80e60736915d3ec70d31/osx_utils.sh -s -o osx_utils.sh
 source osx_utils.sh
 
 get_macpython_environment ${PYTHON_VERSION} venv "10.6"
